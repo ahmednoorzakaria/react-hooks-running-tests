5cb28f2f53c6fe31d6acd8a5e36f19b24b06cbd6
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-07-18 17:28:25 +0300

    components lab

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+function Header() {
+    return <h1>hello from the Header!</h1>;
+  }
+  function Article() {
+    return <div>please pass this test</div>;
+  }
